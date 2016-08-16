8cf4dfd56b3ce42813e169870cb682598ccaa37a
Alexey <akar.14.92@gamil.com>
2016-08-16 19:19:29 +0300

    RRMS error comparison FE vs SIE

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from __future__ import division
 import matplotlib
-from matplotlib import pylab as pl
+import matplotlib.pyplot as pl
 import numpy as np
 import time
 
@@ -20,10 +20,10 @@ def beta_n(v):  return 0.125 * np.exp(-v / 80)
 def n_inf(v): return alpha_n(v) / (alpha_n(v) + beta_n(v))
 
 
-def alpha_m(v): return 0.1 * (-v + 25) / (np.exp((-v + 25) / 10) - 1) if v != 25 else 1
+def alpha_m(v): return (1) * 0.1 * (-v + 25) / (np.exp((-v + 25) / 10) - 1) if v != 25 else 5
 
 
-def beta_m(v): return 4 * np.exp(-v / 18)
+def beta_m(v): return (1.)*4 * np.exp(-v / 18)
 
 
 def m_inf(v): return alpha_m(v) / (alpha_m(v) + beta_m(v))
@@ -38,22 +38,38 @@ def beta_h(v): return 1 / (np.exp((-v + 30) / 10) + 1)
 def h_inf(v): return alpha_h(v) / (alpha_h(v) + beta_h(v))
 
 
+
+
+
 def CalculateNorm(arrayAnalitical, array2):
     k = len(arrayAnalitical) / len(array2)
     normArray = []
+
+    # calculating RRHS error
+    numerator, denumerator = 0, 0
     for i in range(len(array2)):
+        numerator += (arrayAnalitical[k*i] - array2[i])**2
+        denumerator += (arrayAnalitical[k*i])**2
+        '''
         delta = abs(arrayAnalitical[int(k * i)] - array2[i])
         normArray.append(delta)
+        '''
+    RRHS_error = np.sqrt(numerator / denumerator)
+    #
+    print 100*RRHS_error
+    '''
     normArray = np.array(normArray)
     max = np.amax(normArray)
+    amplitude = np.amax(normArray) - np.amin(normArray)
     indexMax = np.argmax(normArray)
-    return max, indexMax
+    '''
+    return 100*RRHS_error
 
 
 
 
 
-g_n = 120
+g_n = 1200
 g_k = 36
 g_l = 0.3
 v_n = 115
@@ -61,10 +77,10 @@ v_k = -12
 v_l = 10.613
 c = 1
 
-dtArray = np.linspace(1e-4, 5e-2, 80)
+dtArray = np.linspace(1e-3, 3e-2, 50)
 
 print dtArray
-T = 5.0
+T = 50.0
 
 
 def rhs(i_s, m_, n_, h_, v_):
@@ -95,13 +111,23 @@ timeEuler = time.clock() - startEuler
 '''
 
 error, error_e, timeMaxError, timeMaxError_e = [], [], [], []
-computationalTimesFE, computationalTimesSIE = [], []
-
+amplitude = 0
 counter = 0
 omega = 1 # relaxation parameter
 pl.figure()
+
+
+
+
+
 for dt in dtArray:
+
+
+
+
     startMine = time.clock()
+
+
 
     # grid arrays and parameters
     time_array = np.arange(0, T, dt)
@@ -111,9 +137,9 @@ for dt in dtArray:
     if counter == 0:
         analiticalSolution = np.zeros(len(time_array))
 
+    #calculateActionPotential(m, n, h, )
 
-
-    m[0] = m_e[0] =  m_inf(v_rest)
+    m[0] = m_e[0] = m_inf(v_rest)
     h[0] = h_e[0] = h_inf(v_rest)
     n[0] = n_e[0] = n_inf(v_rest)
     v[0] = v_e[0] = v_rest
@@ -121,47 +147,7 @@ for dt in dtArray:
     I_s = np.zeros(len(time_array))
     I_s[:] = 10
 
-    startFE = time.clock()
     dtStar = dt / 2
-    for i in range(1, len(time_array)):
-
-        #m[i] = m_inf(v[i-1]) + (m[i-1] - m_inf(v[i-1])) * np.exp(-dt * (alpha_m(v[i-1]) + beta_m(v[i-1])))
-        #n[i] = n_inf(v[i-1]) + (n[i-1] - n_inf(v[i-1])) * np.exp(-dt * (alpha_n(v[i-1]) + beta_n(v[i-1])))
-        #h[i] = h_inf(v[i-1]) + (h[i-1] - h_inf(v[i-1])) * np.exp(-dt * (alpha_h(v[i-1]) + beta_h(v[i-1])))
-
-
-        m_e[i] = m_inf(v_e[i-1]) + (m_e[i-1] - m_inf(v_e[i-1])) * np.exp(-dt * (alpha_m(v_e[i-1]) + beta_m(v_e[i-1])))
-        n_e[i] = n_inf(v_e[i-1]) + (n_e[i-1] - n_inf(v_e[i-1])) * np.exp(-dt * (alpha_n(v_e[i-1]) + beta_n(v_e[i-1])))
-        h_e[i] = h_inf(v_e[i-1]) + (h_e[i-1] - h_inf(v_e[i-1])) * np.exp(-dt * (alpha_h(v_e[i-1]) + beta_h(v_e[i-1])))
-
-        # rhs = (1./c) * (I_s[i-1] - g_n*m**3*h*(v[i-1]-v_n) - g_k*n**4*(v[i-1]-v_k) - g_l*(v[i-1]-v_l))
-        #derivative = (rhs(I_s[i - 1], m[i], n[i], h[i], v[i - 1] + d_gating_var) - rhs(I_s[i - 1], m[i], n[i], h[i],
-         #                                                                    v[i - 1])) / d_gating_var
-
-        #Rhs[i-1] = rhs(I_s[i-1], m[i-1], n[i-1], h[i-1], v[i-1])
-        Rhs_e[i-1] = rhs(I_s[i-1], m_e[i-1], n_e[i-1], h_e[i-1], v_e[i-1])
-
-        #if counter != 0: # for calculating numerical solutions with various timesteps
-        #dv = Rhs[i-1] * dt / (1 - omega * dt * derivative)
-        dv_e = Rhs_e[i-1] * dt
-        #else: # for calculating "analitical" solution using Explicit Euler
-        #    dv = Rhs[i-1] * dt
-        #dv_e = Rhs_e[i-1] * dt
-
-        #v[i] = v[i-1] + dv
-        v_e[i] = v_e[i-1] + dv_e
-
-        '''if (i % 2 == 0):
-            dtStar = dt / 1
-        else:
-            dtStar = dt'''
-
-    timeFE = time.clock() - startFE
-    computationalTimesFE.append(timeFE)
-
-
-
-    startSIE = time.clock()
     for i in range(1, len(time_array)):
 
         m[i] = m_inf(v[i-1]) + (m[i-1] - m_inf(v[i-1])) * np.exp(-dt * (alpha_m(v[i-1]) + beta_m(v[i-1])))
@@ -169,37 +155,37 @@ for dt in dtArray:
         h[i] = h_inf(v[i-1]) + (h[i-1] - h_inf(v[i-1])) * np.exp(-dt * (alpha_h(v[i-1]) + beta_h(v[i-1])))
 
 
-        #m_e[i] = m_inf(v_e[i-1]) + (m_e[i-1] - m_inf(v_e[i-1])) * np.exp(-dt * (alpha_m(v_e[i-1]) + beta_m(v_e[i-1])))
-        #n_e[i] = n_inf(v_e[i-1]) + (n_e[i-1] - n_inf(v_e[i-1])) * np.exp(-dt * (alpha_n(v_e[i-1]) + beta_n(v_e[i-1])))
-        #h_e[i] = h_inf(v_e[i-1]) + (h_e[i-1] - h_inf(v_e[i-1])) * np.exp(-dt * (alpha_h(v_e[i-1]) + beta_h(v_e[i-1])))
+        m_e[i] = m_inf(v_e[i-1]) + (m_e[i-1] - m_inf(v_e[i-1])) * np.exp(-dt * (alpha_m(v_e[i-1]) + beta_m(v_e[i-1])))
+        n_e[i] = n_inf(v_e[i-1]) + (n_e[i-1] - n_inf(v_e[i-1])) * np.exp(-dt * (alpha_n(v_e[i-1]) + beta_n(v_e[i-1])))
+        h_e[i] = h_inf(v_e[i-1]) + (h_e[i-1] - h_inf(v_e[i-1])) * np.exp(-dt * (alpha_h(v_e[i-1]) + beta_h(v_e[i-1])))
 
         # rhs = (1./c) * (I_s[i-1] - g_n*m**3*h*(v[i-1]-v_n) - g_k*n**4*(v[i-1]-v_k) - g_l*(v[i-1]-v_l))
         derivative = (rhs(I_s[i - 1], m[i], n[i], h[i], v[i - 1] + d_gating_var) - rhs(I_s[i - 1], m[i], n[i], h[i],
-                                                                             v[i - 1])) / d_gating_var
+                                                                              v[i - 1])) / d_gating_var
 
         Rhs[i-1] = rhs(I_s[i-1], m[i-1], n[i-1], h[i-1], v[i-1])
-        #Rhs_e[i-1] = rhs(I_s[i-1], m_e[i-1], n_e[i-1], h_e[i-1], v_e[i-1])
+        Rhs_e[i-1] = rhs(I_s[i-1], m_e[i-1], n_e[i-1], h_e[i-1], v_e[i-1])
 
-        #if counter != 0: # for calculating numerical solutions with various timesteps
-        dv = Rhs[i-1] * dt / (1 - omega * dt * derivative)
-        #dv_e = Rhs_e[i-1] * dt
-        #else: # for calculating "analitical" solution using Explicit Euler
-        #    dv = Rhs[i-1] * dt
-        #dv_e = Rhs_e[i-1] * dt
+        if counter != 0: # for calculating numerical solutions with various timesteps
+            dv = Rhs[i-1] * dt / (1 - omega * dt * derivative)
+            dv_e = Rhs_e[i-1] * dt
+        else: # for calculating "analitical" solution using Explicit Euler
+            dv = Rhs[i-1] * dt
+            dv_e = Rhs_e[i-1] * dt
 
         v[i] = v[i-1] + dv
-        #v_e[i] = v_e[i-1] + dv_e
+        v_e[i] = v_e[i-1] + dv_e
 
         '''if (i % 2 == 0):
             dtStar = dt / 1
         else:
-        '''
-    timeSIE = time.clock() - startSIE
-    computationalTimesSIE.append(timeSIE)
+            dtStar = dt'''
 
     if (counter == 0):
         analiticalSolution[:] = v[:]
+        amplitude = np.amin(analiticalSolution)
         #print 'ana = ', analiticalSolution
+        analiticalSolution[:] -= amplitude
 
 
             # dv = rhs(I_s[i - 1], m, n, h, v_euler[i - 1]) * dt
@@ -207,20 +193,27 @@ for dt in dtArray:
     Rhs[-1] = Rhs[-2]
     Rhs_e[-1] = Rhs_e[-2]
 
+    timeMine = time.clock() - startMine
 
-    #errTmp, indexErrTpm = CalculateNorm(analiticalSolution, v_e)
-    #errTmp_e, indexErrTpm_e = CalculateNorm(analiticalSolution, v_e)
 
-    #error.append(errTmp)
-    #error_e.append(errTmp_e)
+    print "amplitude %.2e" % amplitude
+    v[:] -= amplitude
+    v_e[:] -= amplitude
+
+
+    errTmp = CalculateNorm(analiticalSolution, v)
+    errTmp_e = CalculateNorm(analiticalSolution, v_e)
+
+    error.append(errTmp)
+    error_e.append(errTmp_e)
     #timeMaxError.append(indexErrTpm * dt)
 
     if counter == 0:
         pl.plot(time_array, v, '--k', label='analytical (dt = %.2e ms)' % dtArray[counter], linewidth=5)
-    elif (counter%2 == 0):
+    elif (counter%3 == 0):
         pl.plot(time_array, v, '-', label='dt = %.2e ms' % dtArray[counter], linewidth=3)
     #pl.title('timestep = %.0e ms\n' % (dt))
-    pl.legend(loc='lower left')
+    pl.legend(loc='upper right')
     pl.xlabel('time, ms')
     pl.ylabel('action potential, mV')
     #pl.ylim([-20, 120])
@@ -230,41 +223,43 @@ for dt in dtArray:
 
 #np.savetxt('error_derivative_explicit_euler.csv', np.c_[dtArray[1:], error[1:], timeMaxError[1:]], delimiter=',', header='timestep(ms),max_norm_error,time_of_max_norm_error(ms)')
 f, (ax1) = pl.subplots(1, 1)
-'''ax1.plot(np.log(dtArray[1:]), np.log(error[1:]), 'k-o', linewidth=4, markersize = 15)
-ax1.set_xlabel('ln(timestep)')
-ax1.set_ylabel('ln(absolute error)')
-ax1.grid('on')'''
-#ax1.plot((error[1:]), dtArray[1:], 'bo', label='Simplified Backward Euler', linewidth=4, markersize = 15)
-computationalTimePercent = 100*np.abs(np.array(computationalTimesFE[:]) - np.array(computationalTimesSIE[:])) / np.array(computationalTimesFE[:])
-ax1.plot(dtArray[:], computationalTimePercent ,'ko', linewidth=4, markersize = 15)
-ax1.plot(dtArray[:], np.array([np.mean(computationalTimePercent) for i in xrange(len(dtArray))]), 'k--', linewidth = 14)
-#ax1.plot(dtArray[:], np.array(computationalTimesSIE[:]) ,'bo', label='Simplified Backward Euler', linewidth=4, markersize = 15)
-#ax1.set_ylim([9, 15])
+#ax1.plot(np.log(dtArray[1:]), np.log(error[1:]), 'k-o', linewidth=4, markersize = 15)
+#ax1.set_xlabel('ln(timestep)')
+#ax1.set_ylabel('ln(absolute error)')
+ax1.grid('on')
+ax1.plot((dtArray[1:]), (error[1:]), 'b-o', label='Simplified Backward Euler', linewidth=4, markersize = 10)
+ax1.plot((dtArray[1:]), (error_e[1:]), 'g-o', label='Forward Euler', linewidth=4, markersize = 10)
+#ax1.set_ylim([0, 50])
+extraticks = [5]
+#ax1.set_xticks(list(ax1.xticks()[0]) + extraticks)
+ax1.axhline(y=5, linewidth=4, color='r')
+
+
 '''
 # calc the trendline
 z = np.polyfit(dtArray[1:7], error[1:7], 1)
 p = np.poly1d(z)
-#ax1.plot((dtArray[1:]), p((dtArray[1:])),"b-", linewidth=4)
+ax1.plot((dtArray[1:]), p((dtArray[1:])),"b-", linewidth=4)
 # the line equation:
 
-#ax1.text(0.01, 45e1, "y = (%.2e)*x + (%.2e)" %(z[0], z[1]), color='b', fontsize = 20, fontweight='bold')
+ax1.text(0.01, 45e1, "y = (%.2e)*x + (%.2e)" %(z[0], z[1]), color='b', fontsize = 20, fontweight='bold')
 
 # calc the trendline
 z_e = np.polyfit(dtArray[1:7], error_e[1:7], 1)
 p_e = np.poly1d(z_e)
-#ax1.plot((dtArray[1:]), p_e((dtArray[1:])),"g-", linewidth=4)
+ax1.plot((dtArray[1:]), p_e((dtArray[1:])),"g-", linewidth=4)
 # the line equation:
-#ax1.text(0.01, 40e1, "y = (%.2e)*x + (%.2e)" %(z_e[0], z_e[1]), color='g', fontsize = 20, fontweight='bold')
+ax1.text(0.01, 40e1, "y = (%.2e)*x + (%.2e)" %(z_e[0], z_e[1]), color='g', fontsize = 20, fontweight='bold')
 '''
 ax1.set_xlabel('timestep, ms')
-ax1.set_ylabel('%')
+ax1.set_ylabel('RRMS error, %')
 ax1.grid('on')
-ax1.legend(loc='upper right')
+#ax1.legend(loc='upper left')
 # f.figure()
 
 #np.savetxt('error_explicit_euler.csv', np.c_[dtArray[1:], error[1:], timeMaxError[1:]], delimiter=',', header='timestep(ms),max_norm_error,time_of_max_norm_error(ms)')
-
 '''
+
 pl.figure()
 startMine = time.clock()
 for i in range(1, len(time_array)):
