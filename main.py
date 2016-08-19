e50f6ff5115d73262667b3193b60724c6d9622ea
Alexey <akar.14.92@gamil.com>
2016-08-19 22:23:10 +0300

    more optimized JITting

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import matplotlib
 import matplotlib.pyplot as pl
 import numpy as np
 import time
-from numba import jit
+import numba as nb
 import math as mt
 
 
@@ -13,31 +13,31 @@ font = {'family': 'normal',
 
 matplotlib.rc('font', **font)
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def alpha_n(val): return 0.01 * (-val + 10) / (mt.exp((-val + 10) / 10) - 1) if val != 10 else 0.1
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def beta_n(val):  return 0.125 * mt.exp(-val / 80)
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def n_inf(val): return alpha_n(val) / (alpha_n(val) + beta_n(val))
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def alpha_m(val): return (1) * 0.1 * (-val + 25) / (mt.exp((-val + 25) / 10) - 1) if val != 25 else 5
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def beta_m(val): return (1.)*4 * mt.exp(-val / 18)
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def m_inf(val): return alpha_m(val) / (alpha_m(val) + beta_m(val))
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def alpha_h(val): return 0.07 * mt.exp(-val / 20)
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def beta_h(val): return 1 / (mt.exp((-val + 30) / 10) + 1)
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def h_inf(val): return alpha_h(val) / (alpha_h(val) + beta_h(val))
 
 
@@ -53,26 +53,24 @@ def CalculateNorm(arrayAnalitical, array2):
     for i in range(len(array2)):
         numerator += (arrayAnalitical[k*i] - array2[i])**2
         denumerator += (arrayAnalitical[k*i])**2
-        '''
+
         delta = abs(arrayAnalitical[int(k * i)] - array2[i])
         normArray.append(delta)
-        '''
+
     RRHS_error = np.sqrt(numerator / denumerator)
-    #
-    #print 100*RRHS_error
-    '''
+
+    # calculating max|o| error
     normArray = np.array(normArray)
-    max = np.amax(normArray)
-    amplitude = np.amax(normArray) - np.amin(normArray)
+    max_module = np.amax(normArray)
+    amplitude = np.amax(arrayAnalitical) - np.amin(arrayAnalitical)
     indexMax = np.argmax(normArray)
-    '''
-    return 100*RRHS_error
+
+    return 100 * RRHS_error, 100*max_module / amplitude, 100 * max_module / np.sqrt(( (len(arrayAnalitical))**(-1)) * denumerator)
 
 
 
 
-
-g_n = 300
+g_n = 1200
 g_k = 36
 g_l = 0.3
 v_n = 115
@@ -80,16 +78,16 @@ v_k = -12
 v_l = 10.613
 c = 1
 
-dtArray = np.linspace(1e-4, 1e-2, 50)
+dtArray = np.linspace(1e-5, 1e-3, 50)
 
 print dtArray
 T = 50.0
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def rhs(i_s, m_, n_, h_, v_):
     return (1. / c) * (i_s - g_n * (m_) ** 3 * (h_) * (v_ - v_n) - g_k * (n_ ** 4) * (v_ - v_k) - g_l * (v_ - v_l))
 
-@jit(nopython=True)
+@nb.jit(nopython=True)
 def rhs_gating_vars(alpha_, beta_, v_, var_):
     return alpha_(v_) * (1 - var_) - beta_(v_) * var_
 
@@ -118,37 +116,39 @@ amplitude = 0
 counter = 0
 omega = 1 # relaxation parameter
 
+@nb.jit((nb.float64[:], nb.int64, nb.float64, nb.int32, \
+                        nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:], \
+                        nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:], \
+                        nb.float64[:]), nopython=True)
+def CalculateHH(time_array, size, dt, counter, m, n, h, v, Rhs, m_e, n_e, h_e, v_e, Rhs_e, I_s):
 
-@jit(nopython=True)
-def CalculateHH(dt):
-    time_array = np.arange(0, T, dt)
-    v, m, n, h, Rhs = np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array))
+    '''
+    v, m, n, h, Rhs = [0. for i in range(size)], [0. for i in range(size)], [0. for i in range(size)], [0. for i in range(size)], [0. for i in range(size)],
+    #np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array))
     v_e, m_e, n_e, h_e, Rhs_e = np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array))
     I_s = np.zeros(len(time_array))
-
+    '''
 
     m[0] = m_e[0] = m_inf(v_rest)
     h[0] = h_e[0] = h_inf(v_rest)
     n[0] = n_e[0] = n_inf(v_rest)
     v[0] = v_e[0] = v_rest
 
-    I_s[:] = 10
 
     dtStar = dt / 2
-    for i in range(1, len(time_array)):
+    for i in range(1, size):
 
-        m[i] = m_inf(v[i-1]) + (m[i-1] - m_inf(v[i-1])) * mt.exp(-dt * (alpha_m(v[i-1]) + beta_m(v[i-1])))
-        n[i] = n_inf(v[i-1]) + (n[i-1] - n_inf(v[i-1])) * mt.exp(-dt * (alpha_n(v[i-1]) + beta_n(v[i-1])))
-        h[i] = h_inf(v[i-1]) + (h[i-1] - h_inf(v[i-1])) * mt.exp(-dt * (alpha_h(v[i-1]) + beta_h(v[i-1])))
+        m[i] = m_inf(v[i-1]) + (m[i-1] - m_inf(v[i-1])) * np.exp(-dt * (alpha_m(v[i-1]) + beta_m(v[i-1])))
+        n[i] = n_inf(v[i-1]) + (n[i-1] - n_inf(v[i-1])) * np.exp(-dt * (alpha_n(v[i-1]) + beta_n(v[i-1])))
+        h[i] = h_inf(v[i-1]) + (h[i-1] - h_inf(v[i-1])) * np.exp(-dt * (alpha_h(v[i-1]) + beta_h(v[i-1])))
 
 
-        m_e[i] = m_inf(v_e[i-1]) + (m_e[i-1] - m_inf(v_e[i-1])) * mt.exp(-dt * (alpha_m(v_e[i-1]) + beta_m(v_e[i-1])))
-        n_e[i] = n_inf(v_e[i-1]) + (n_e[i-1] - n_inf(v_e[i-1])) * mt.exp(-dt * (alpha_n(v_e[i-1]) + beta_n(v_e[i-1])))
-        h_e[i] = h_inf(v_e[i-1]) + (h_e[i-1] - h_inf(v_e[i-1])) * mt.exp(-dt * (alpha_h(v_e[i-1]) + beta_h(v_e[i-1])))
+        m_e[i] = m_inf(v_e[i-1]) + (m_e[i-1] - m_inf(v_e[i-1])) * np.exp(-dt * (alpha_m(v_e[i-1]) + beta_m(v_e[i-1])))
+        n_e[i] = n_inf(v_e[i-1]) + (n_e[i-1] - n_inf(v_e[i-1])) * np.exp(-dt * (alpha_n(v_e[i-1]) + beta_n(v_e[i-1])))
+        h_e[i] = h_inf(v_e[i-1]) + (h_e[i-1] - h_inf(v_e[i-1])) * np.exp(-dt * (alpha_h(v_e[i-1]) + beta_h(v_e[i-1])))
 
         # rhs = (1./c) * (I_s[i-1] - g_n*m**3*h*(v[i-1]-v_n) - g_k*n**4*(v[i-1]-v_k) - g_l*(v[i-1]-v_l))
-        derivative = (rhs(I_s[i - 1], m[i], n[i], h[i], v[i - 1] + d_gating_var) - rhs(I_s[i - 1], m[i], n[i], h[i],
-                                                                              v[i - 1])) / d_gating_var
+        derivative = (rhs(I_s[i - 1], m[i], n[i], h[i], v[i - 1] + d_gating_var) - rhs(I_s[i - 1], m[i], n[i], h[i], v[i - 1])) / d_gating_var
 
         Rhs[i-1] = rhs(I_s[i-1], m[i-1], n[i-1], h[i-1], v[i-1])
         Rhs_e[i-1] = rhs(I_s[i-1], m_e[i-1], n_e[i-1], h_e[i-1], v_e[i-1])
@@ -163,7 +163,6 @@ def CalculateHH(dt):
         v[i] = v[i-1] + dv
         v_e[i] = v_e[i-1] + dv_e
 
-    return time_array, v, v_e
 
 
 
@@ -175,13 +174,28 @@ for dt in dtArray:
 
     # grid arrays and parameters
     '''
-    time_array = np.arange(0, T, dt)
+    time_array = np.zeros(SIZE)
     v, m, n, h, Rhs = np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array))
     v_e, m_e, n_e, h_e, Rhs_e = np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array))
     I_s = np.zeros(len(time_array))
     '''
+    time_array = np.arange(0, T, dt)
+    SIZE = len(time_array)
+    v = np.zeros(SIZE)
+    m = np.zeros(SIZE)
+    n = np.zeros(SIZE)
+    h = np.zeros(SIZE)
+    Rhs = np.zeros(SIZE) # [0. for i in range(size)]
+    v_e = np.zeros(SIZE) #[0. for i in range(size)]
+    m_e = np.zeros(SIZE) #[0. for i in range(size)]
+    h_e = np.zeros(SIZE) # [0. for i in range(size)]
+    n_e = np.zeros(SIZE) #[0. for i in range(size)]
+    Rhs_e = np.zeros(SIZE) #[0. for i in range(size)]
+    I_s = np.zeros(SIZE) #[10. for i in range(size)]
 
-    time_array, V, V_e = CalculateHH(dt)
+    CalculateHH(time_array, SIZE, dt, counter, m, n, h, v, Rhs, m_e, n_e, h_e, v_e, Rhs_e, I_s)
+
+
     #time_array = np.array(time_array)
     #V = np.array(time_array)
     #V_e = np.array(time_array)
@@ -232,10 +246,11 @@ for dt in dtArray:
         if (i % 2 == 0):
             dtStar = dt / 1
         else:
-            dtStar = dt'''
+            dtStar = dt
+    '''
 
     if (counter == 0):
-        analiticalSolution[:] = V[:]
+        analiticalSolution[:] = v[:]
         amplitude = np.amin(analiticalSolution)
         #print 'ana = ', analiticalSolution
         analiticalSolution[:] -= amplitude
@@ -250,21 +265,21 @@ for dt in dtArray:
     #print timeMine
 
     #print "amplitude %.2e" % amplitude
-    V[:] -= amplitude
-    V_e[:] -= amplitude
+    v[:] -= amplitude
+    v_e[:] -= amplitude
 
 
-    errTmp = CalculateNorm(analiticalSolution, V)
-    errTmp_e = CalculateNorm(analiticalSolution, V_e)
+    errTmp = CalculateNorm(analiticalSolution, v)
+    errTmp_e = CalculateNorm(analiticalSolution, v_e)
 
-    error.append(errTmp)
-    error_e.append(errTmp_e)
+    error.append(0)
+    error_e.append(0)
     #timeMaxError.append(indexErrTpm * dt)
 
     if counter == 0:
         pl.plot(time_array, analiticalSolution, '--k', label='analytical (dt = %.2e ms)' % dtArray[counter], linewidth=5)
     elif (counter%3 == 0):
-        pl.plot(time_array, V_e, '-', label='dt = %.2e ms' % dtArray[counter], linewidth=3)
+        pl.plot(time_array, v, '-', label='dt = %.2e ms' % dtArray[counter], linewidth=3)
     #pl.title('timestep = %.0e ms\n' % (dt))
     pl.legend(loc='upper right')
     pl.xlabel('time, ms')
@@ -273,20 +288,34 @@ for dt in dtArray:
     pl.grid('on')
     counter += 1
 
-print time.clock() - start
-
+print 'time elapsed = %.2e sec' % (time.clock() - start)
+error = map(list, zip(*error))
+error_e = map(list, zip(*error_e))
 #np.savetxt('error_derivative_explicit_euler.csv', np.c_[dtArray[1:], error[1:], timeMaxError[1:]], delimiter=',', header='timestep(ms),max_norm_error,time_of_max_norm_error(ms)')
 f, (ax1) = pl.subplots(1, 1)
 #ax1.plot(np.log(dtArray[1:]), np.log(error[1:]), 'k-o', linewidth=4, markersize = 15)
 #ax1.set_xlabel('ln(timestep)')
 #ax1.set_ylabel('ln(absolute error)')
 ax1.grid('on')
-ax1.plot((dtArray[1:]), (error[1:]), 'b-o', label='Simplified Backward Euler', linewidth=4, markersize = 10)
-ax1.plot((dtArray[1:]), (error_e[1:]), 'g-o', label='Forward Euler', linewidth=4, markersize = 10)
-#ax1.set_ylim([0, 50])
+ax1.plot((dtArray[1:]), (error[0][1:]), 'b-o', label='Simplified Backward Euler', linewidth=4, markersize = 10)
+ax1.plot((dtArray[1:]), (error_e[0][1:]), 'g-o', label='Forward Euler', linewidth=4, markersize = 10)
+ax1.legend()
+
+ax1.plot((dtArray[1:]), (error[1][1:]), 'b-s', label='Simplified Backward Euler', linewidth=4, markersize = 10)
+ax1.plot((dtArray[1:]), (error_e[1][1:]), 'g-s', label='Forward Euler', linewidth=4, markersize = 10)
+ax1.grid('on')
+#ax2.legend()
+
+ax1.plot((dtArray[1:]), (error[2][1:]), 'b-v', label='Simplified Backward Euler', linewidth=4, markersize = 10)
+ax1.plot((dtArray[1:]), (error_e[2][1:]), 'g-v', label='Forward Euler', linewidth=4, markersize = 10)
+#ax1.grid('on')
+#ax3.legend()
+ax1.set_ylim([0, 15])
 extraticks = [5]
 #ax1.set_xticks(list(ax1.xticks()[0]) + extraticks)
 ax1.axhline(y=5, linewidth=4, color='r')
+#ax2.axhline(y=5, linewidth=4, color='r')
+#ax3.axhline(y=5, linewidth=4, color='r')
 
 
 '''
@@ -308,7 +337,7 @@ ax1.text(0.01, 40e1, "y = (%.2e)*x + (%.2e)" %(z_e[0], z_e[1]), color='g', fonts
 ax1.set_xlabel('timestep, ms')
 ax1.set_ylabel('RRMS error, %')
 ax1.grid('on')
-#ax1.legend(loc='upper left')
+ax1.legend(loc='upper left')
 # f.figure()
 
 #np.savetxt('error_explicit_euler.csv', np.c_[dtArray[1:], error[1:], timeMaxError[1:]], delimiter=',', header='timestep(ms),max_norm_error,time_of_max_norm_error(ms)')
@@ -355,4 +384,4 @@ pl.legend(('voltage spiking','input signal strength'),"upper right")
 pl.xlabel("time, s --->")
 pl.ylabel("potential difference,V (mV) ---->")
 '''
-pl.show()
+#pl.show()
