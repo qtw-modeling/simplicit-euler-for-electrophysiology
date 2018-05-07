1097090cebca5879a53da5d0964ef95ce2e29ea6
quothewhite <akar.14.92@gmail.com>
2018-05-07 17:37:47 +0300

    Errors estimation

--- main.py
+++ main.py
@@ -1,18 +1,21 @@
 from __future__ import division
 import matplotlib
-import matplotlib.pyplot as pl
+import matplotlib.pyplot as plt
 import numpy as np
 import time
 import numba as nb
 import math as mt
 #from forErrorEtimation import *
 
+#matplotlib.rc('xtick', labelsize=20)
+#matplotlib.rc('ytick', labelsize=20)
+matplotlib.rcParams.update({'font.size': 30})
 
-font = {'family': 'normal',
-        'weight': 'normal',
-        'size': 20}
+#font = {'family': 'normal',
+    #    'weight': 'normal',
+     #   'size': 20}
 
-matplotlib.rc('font', **font)
+#matplotlib.rc('font', **font)
 
 @nb.jit(nopython=True)
 def alpha_n(val): return 0.01 * (-val + 10) / (mt.exp((-val + 10) / 10) - 1) if val != 10 else 0.1
@@ -24,19 +27,19 @@ def beta_n(val):  return 0.125 * mt.exp(-val / 80)
 def n_inf(val): return alpha_n(val) / (alpha_n(val) + beta_n(val))
 
 @nb.jit(nopython=True)
-def alpha_m(val): return (10) * 0.1 * (-val + 25) / (mt.exp((-val + 25) / 10) - 1) if val != 25 else 5
+def alpha_m(val): return 0.1 * (-val + 25) / (mt.exp((-val + 25) / 10) - 1) if val != 25 else 5
 
 @nb.jit(nopython=True)
-def beta_m(val): return (10.)*4 * mt.exp(-val / 18)
+def beta_m(val): return 4 * mt.exp(-val / 18)
 
 @nb.jit(nopython=True)
 def m_inf(val): return alpha_m(val) / (alpha_m(val) + beta_m(val))
 
 @nb.jit(nopython=True)
-def alpha_h(val): return 5e-1*0.07 * mt.exp(-val / 20)
+def alpha_h(val): return 0.07 * mt.exp(-val / 20)
 
 @nb.jit(nopython=True)
-def beta_h(val): return 5e-1*1 / (mt.exp((-val + 30) / 10) + 1)
+def beta_h(val): return 1 / (mt.exp((-val + 30) / 10) + 1)
 
 @nb.jit(nopython=True)
 def h_inf(val): return alpha_h(val) / (alpha_h(val) + beta_h(val))
@@ -67,18 +70,17 @@ def CalculateNorm(arrayAnalitical, array2):
     max_module = np.amax(normArray)
     amplitude = np.amax(arrayAnalitical) - np.amin(arrayAnalitical)
     indexMax = np.argmax(normArray)
+    errors_list = [100 * RRHS_error, 100*max_module / amplitude]
 
-    returnList = list((100 * RRHS_error, max_module, 100 * max_module / np.sqrt(( (len(arrayAnalitical))**(-1)) * denumerator)))
-    for i in range(3):
-        if np.isnan(returnList[i]) == True:
-            returnList[i] = 100500
+    '''for i in range(len(errors_list)):
+        if np.isnan(errors_list[i]) == True:
+            errors_list[i] = 1e9'''
 
-    return returnList
-
-
+    return errors_list
 
 
-g_n = 1200
+
+g_n = 1200#120
 g_k = 36
 g_l = 0.3
 v_n = 115
@@ -86,19 +88,17 @@ v_k = -12
 v_l = 10.613
 c = 1
 
-
-dtArray = np.array([0.5*2**n for n in range(-15, 0, 1)])
+dtArray = [2**(-n) for n in range(17, 0, -1)] #16 points - for error estimation
 
 print dtArray
-T = 30.
-
+T = 6
 @nb.jit(nopython=True)
 def rhs(i_s, m_, n_, h_, v_, c_):
     return (1. / c_) * (i_s - g_n * (m_) ** 3 * (h_) * (v_ - v_n) - g_k * (n_ ** 4) * (v_ - v_k) - g_l * (v_ - v_l))
 
 @nb.jit(nopython=True)
 def rhs_gating_vars(alpha_, beta_, v_, var_):
-    return alpha_ * (1 - var_) - beta_ * var_
+    return alpha_(v_) * (1 - var_) - beta_(v_) * var_
 
 
 step_v = 1e-4
@@ -123,7 +123,7 @@ timeEuler = time.clock() - startEuler
 error, error_e, timeMaxError, timeMaxError_e = [], [], [], []
 amplitude = 0
 counter = 0
-omega = 0.5 # relaxation parameter
+omega = 1 # relaxation parameter
 
 @nb.jit((nb.float64[:], nb.int64, nb.float64, nb.int64, \
                         nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:], nb.float64[:], \
@@ -163,45 +163,24 @@ def CalculateHHusingSImplicitEuler(time_array, size, dt, counter, m, n, h, v, Rh
     v[0] = v_rest
 
 
-    dtStar = dt / 2.
+    dtStar = dt / 2
     for i in xrange(1, size):
+        m[i] = m_inf(v[i-1]) + (m[i-1] - m_inf(v[i-1])) * np.exp(-dt * (alpha_m(v[i-1]) + beta_m(v[i-1])))
+        n[i] = n_inf(v[i-1]) + (n[i-1] - n_inf(v[i-1])) * np.exp(-dt * (alpha_n(v[i-1]) + beta_n(v[i-1])))
+        h[i] = h_inf(v[i-1]) + (h[i-1] - h_inf(v[i-1])) * np.exp(-dt * (alpha_h(v[i-1]) + beta_h(v[i-1])))
 
-        derivative_m = 1. / 1e-5 * (rhs_gating_vars(alpha_m(v[i - 1]), beta_m(v[i - 1]), v[i - 1], m[i - 1] + 1e-5) \
-                                    - rhs_gating_vars(alpha_m(v[i - 1]), beta_m(v[i - 1]), v[i - 1], m[i - 1]))
-        derivative_n = 1. / 1e-5 * (rhs_gating_vars(alpha_n(v[i - 1]), beta_n(v[i - 1]), v[i - 1], n[i - 1] + 1e-5) \
-                                    - rhs_gating_vars(alpha_n(v[i - 1]), beta_n(v[i - 1]), v[i - 1], n[i - 1]))
-        derivative_h = 1. / 1e-5 * (rhs_gating_vars(alpha_h(v[i - 1]), beta_h(v[i - 1]), v[i - 1], h[i - 1] + 1e-5) \
-                                    - rhs_gating_vars(alpha_h(v[i - 1]), beta_h(v[i - 1]), v[i - 1], h[i - 1]))
+        Rhs[i-1] = rhs(I_s[i-1], m[i-1], n[i-1], h[i-1], v[i-1], c)
+        derivative = (rhs(I_s[i-1], m[i-1], n[i-1], h[i-1], v[i-1] + d_gating_var, c) - Rhs[i-1]) / d_gating_var
 
-        # modified Euler stepping for gating vars
-        mStar = m[i - 1] + dt * rhs_gating_vars(alpha_m(v[i - 1]), beta_m(v[i - 1]), v[i - 1], m[i - 1])
-        nStar = n[i - 1] + dt * rhs_gating_vars(alpha_n(v[i - 1]), beta_n(v[i - 1]), v[i - 1], n[i - 1])
-        hStar = h[i - 1] + dt * rhs_gating_vars(alpha_h(v[i - 1]), beta_h(v[i - 1]), v[i - 1], h[i - 1])
-        Rhs[i - 1] = rhs(I_s[i - 1], m[i - 1], n[i - 1], h[i - 1], v[i - 1], c)
-
-
-        derivativeV = (rhs(I_s[i - 1], m[i], n[i], h[i], v[i - 1] + d_gating_var, c) - rhs(I_s[i - 1], m[i], n[i], h[i],
-                                                                                           v[i - 1], c)) / d_gating_var
-        derivativeForCROS = (rhs(I_s[i], m[i], n[i], h[i], v[i - 1] + d_gating_var, c) - rhs(I_s[i], m[i], n[i], h[i],
-                                                                                             v[i - 1],
-                                                                                             c)) / d_gating_var
-
-        gamma = omega * dt * derivativeForCROS   # gamma --- coefficient for short form
-        ReK = Rhs[i - 1]*(1 - gamma)/(1 - 2*gamma + 2*gamma**2)  # CROS scheme
-        #dv = dt * ReK
-        vStar = v[i - 1] + dt * Rhs[i - 1] * 1. / (1. - omega * dt * derivativeV)
-
-        m[i] = m[i - 1] + dtStar * (rhs_gating_vars(alpha_m(vStar), beta_m(vStar), vStar, mStar) + rhs_gating_vars(alpha_m(v[i - 1]), beta_m(v[i - 1]), v[i - 1], m[i - 1]))
-        n[i] = n[i - 1] + dtStar * (rhs_gating_vars(alpha_n(vStar), beta_n(vStar), vStar, nStar) + rhs_gating_vars(alpha_n(v[i - 1]), beta_n(v[i - 1]), v[i - 1], n[i - 1]))
-        h[i] = h[i - 1] + dtStar * (rhs_gating_vars(alpha_h(vStar), beta_h(vStar), vStar, hStar) + rhs_gating_vars(alpha_h(v[i - 1]), beta_h(v[i - 1]), v[i - 1], h[i - 1]))
-        v[i] = vStar
-
+        dv = Rhs[i-1] * dt / (1 - omega * dt * derivative)
+        v[i] = v[i-1] + dv
 
 
 
 amplitude = 0
 start = time.clock()
-pl.figure()
+plt.figure()
+
 
 
 timingEE, timingSIE = [], []
@@ -215,6 +194,7 @@ for dt in dtArray:
     v_e, m_e, n_e, h_e, Rhs_e = np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array)), np.zeros(len(time_array))
     I_s = np.zeros(len(time_array))
     '''
+    print 'Iteration #%d' % counter
     time_array = np.arange(0, T, dt)
     SIZE = len(time_array)
     v = 0*np.ones(SIZE)
@@ -319,15 +299,15 @@ for dt in dtArray:
     #timeMaxError.append(indexErrTpm * dt)
 
     if counter == 0:
-        pl.plot(time_array, analiticalSolution, '--k', label='Analytical' % dtArray[counter], linewidth=5)
-    elif (counter%3 == 0):
-        pl.plot(time_array, v, '-', label='Step = %.2e ms' % dtArray[counter], linewidth=3)
+        plt.plot(time_array, analiticalSolution, '--k', label='Analytical (dt = %.2e ms)' % dtArray[counter], linewidth=5)
+    elif (counter%4 == 0):
+        plt.plot(time_array, v, '-', label='dt = %.2e ms' % dtArray[counter], linewidth=3)
     #pl.title('timestep = %.0e ms\n' % (dt))
-    pl.legend(loc='best')
-    pl.xlabel('Time, ms')
-    pl.ylabel('Membrane potential, mV')
-    pl.ylim([-20, 140])
-    pl.grid('on')
+    plt.legend(loc='upper right')
+    plt.xlabel('Time, ms')
+    plt.ylabel('Membrane potential, mV')
+    plt.ylim([0, 140])
+    plt.grid('on')
     counter += 1
 #pl.show()
 print 'time elapsed = %.2e sec' % (time.clock() - start)
@@ -338,7 +318,7 @@ def CalculatePolyfit(arrayX, arrayY, order):
     return p # is function of polynom type
 
 
-left, right = 2, 20
+left, right = 2, 9
 orderOfpolynomial = 1
 # calc the trendline
 '''for i in range(3):
@@ -361,17 +341,18 @@ computational_time_SIE_list = np.array([np.zeros(LEN) for i in range(3)])
 computational_time_EE_list = np.array([np.zeros(LEN) for i in range(3)])
 speedup_list = np.array([np.zeros(LEN) for i in range(3)])
 
+'''
 # loop for error types
 for i in range(NUM_ERROR_TYPES):
     dt_for_errors_list[i] = CalculatePolyfit(error[left:right,i], dtArray[left:right], 1)(list_of_errors)
     dt_for_errors_list_e[i] = CalculatePolyfit(error_e[left:right,i], dtArray[left:right], 1)(list_of_errors)
 
 for i in range(NUM_ERROR_TYPES):
-    computational_time_SIE_list[i] = CalculatePolyfit(dtArray[left:right], timingSIE[left:right], 1)(dt_for_errors_list[:,i])
-    computational_time_EE_list[i] = CalculatePolyfit(dtArray[left:right], timingEE[left:right], 1)(dt_for_errors_list_e[:,i])
+    computational_time_SIE_list[i] = CalculatePolyfit(dtArray[left:right], timingSIE[left:right], 1)(dt_for_errors_list[i,:])
+    computational_time_EE_list[i] = CalculatePolyfit(dtArray[left:right], timingEE[left:right], 1)(dt_for_errors_list_e[i,:])
 
-np.array(computational_time_SIE_list)
-np.array(computational_time_SIE_list)
+computational_time_EE_list = np.array(computational_time_EE_list)
+computational_time_SIE_list = np.array(computational_time_SIE_list)
 
 speedup_list = np.zeros((NUM_ERROR_TYPES, LEN))
 for i in range(NUM_ERROR_TYPES):
@@ -380,13 +361,17 @@ for i in range(NUM_ERROR_TYPES):
         speedup_list[i ,j] = dt_for_errors_list[i,j] / dt_for_errors_list_e[i,j] / 1.1
 #print speedup_list
 
-np.savetxt('speedup_T%.1fsec.csv' % T, np.c_[list_of_errors,\
+
+
+np.savetxt('data_T%.1fsec.csv' % T, np.c_[list_of_errors,\
                                              dt_for_errors_list_e[0,:], dt_for_errors_list[0,:],\
                                              dt_for_errors_list_e[1,:], dt_for_errors_list[1,:],\
                                              dt_for_errors_list_e[2,:], dt_for_errors_list[2,:],\
-                                            speedup_list[0,:], speedup_list[1,:], speedup_list[2,:]],\
+                                            computational_time_EE_list[0,:], computational_time_SIE_list[0,:],\
+                                            computational_time_EE_list[1,:], computational_time_SIE_list[1,:],\
+                                            computational_time_EE_list[2,:], computational_time_SIE_list[2,:]],\
                                             fmt='%.2e', delimiter=',',\
-        header='error,dt_RRMS_EE,dt_RRMS_SIE,dt_maxmod_EE,dt_maxmod_SIE,dt_mixed_EE,dt_mixed_SIE,speedup_RRMS,speedup_maxmod,speedup_mixed')
+        header='error,dt_RRMS_EE,dt_RRMS_SIE,dt_maxmod_EE,dt_maxmod_SIE,dt_mixed_EE,dt_mixed_SIE,CPU_time_RRMS_EE,CPU_time_RRMS_SIE,CPU_time_maxmod_EE,CPU_time_maxmod_SIE,CPU_time_mixed_EE,CPU_time_mixed_SIE')
 #error_e[0,1:], error[1,1:], error_e[1,1], error[2,1:], error_e[2,1:]
 
 np.savetxt('errors_3_types_%.1fsec.csv' % T, np.c_[dtArray[1:],\
@@ -397,16 +382,27 @@ np.savetxt('errors_3_types_%.1fsec.csv' % T, np.c_[dtArray[1:],\
                                                     fmt='%.2e', delimiter=',',\
            header='timestep(ms),RRMS_EE,RRMS_SIE,max_mod_EE, max_mod_SIE,mixed_EE,mixed_SIE,timingEE,timingSIE')
 
+'''
 
-
-f, (ax1) = pl.subplots(1, 1)
+fig2 = plt.figure()
 #ax1.plot(np.log(dtArray[1:]), np.log(error[1:]), 'k-o', linewidth=4, markersize = 15)
 #ax1.set_xlabel('ln(timestep)')
 #ax1.set_ylabel('ln(absolute error)')
-ax1.grid('on')
-ax1.loglog((dtArray[1:]), (error[1:,1]), 'b-o', label='Simplified Backward Euler', linewidth=4, markersize = 10)
-ax1.loglog((dtArray[1:]), (error_e[1:,1]), 'g-o', label='Forward Euler', linewidth=4, markersize = 10)
-ax1.legend()
+plt.grid('on')
+plt.loglog((timingSIE[1:]), (error[1:,0]), 'b-o', linewidth=4, markersize = 10)
+plt.loglog((timingEE[1:]), (error_e[1:,0]), 'g-o', linewidth=4, markersize = 10)
+plt.xlabel('t calc, s')
+plt.ylabel('RRMS error, %')
+plt.axhline(y=5, linewidth=4, color='r')
+
+fig3 = plt.figure()
+plt.grid('on')
+plt.loglog((timingSIE[1:]), (error[1:,1]), 'b-o', linewidth=4, markersize = 10)
+plt.loglog((timingEE[1:]), (error_e[1:,1]), 'g-o', linewidth=4, markersize = 10)
+plt.xlabel('t calc, s')
+plt.ylabel('maxmod error, %')
+plt.axhline(y=5, linewidth=4, color='r')
+
 '''
 ax1.plot((dtArray[1:]), (error[1][1:]), 'b-s', label='Simplified Backward Euler', linewidth=4, markersize = 10)
 ax1.plot((dtArray[1:]), (error_e[1][1:]), 'g-s', label='Forward Euler', linewidth=4, markersize = 10)
@@ -418,15 +414,14 @@ ax1.plot((dtArray[1:]), (error_e[2][1:]), 'g-v', label='Forward Euler', linewidt
 #ax1.grid('on')
 #ax3.legend()
 ax1.set_ylim([0, 15])
-extraticks = [5]'''
+extraticks = [5]
 #ax1.set_xticks(list(ax1.xticks()[0]) + extraticks)
-ax1.axhline(y=5, linewidth=4, color='r')
-ax1.set_ylim([0, 1000])
+'''
+
 #ax2.axhline(y=5, linewidth=4, color='r')
 #ax3.axhline(y=5, linewidth=4, color='r')
-
-
 '''
+
 # calc the trendline
 z = np.polyfit(dtArray[1:7], error[1:7], 1)
 p = np.poly1d(z)
@@ -441,18 +436,15 @@ p_e = np.poly1d(z_e)
 ax1.plot((dtArray[1:]), p_e((dtArray[1:])),"g-", linewidth=4)
 # the line equation:
 ax1.text(0.01, 40e1, "y = (%.2e)*x + (%.2e)" %(z_e[0], z_e[1]), color='g', fontsize = 20, fontweight='bold')
-'''
-ax1.set_xlabel('Step, ms')
-ax1.set_ylabel('Max |.| error, mV')
+
+ax1.set_xlabel('Timestep, ms')
+ax1.set_ylabel('Mixed norm error, %')
 ax1.grid('on')
 ax1.legend(loc='upper left')
 # f.figure()
 
-'''
+
 np.savetxt('errors_3_types_T%.1fsec.csv' % T, np.c_[dtArray[1:], error[0,1:], error_e[0][1:], error[1][1:], error_e[1][1:], error[2][1:], error_e[2][1:], timingSIE[1:], timingEE[1:]], fmt='%.2e', delimiter=',',\
            header='timestep(ms),max_norm_error_SIE,max_norm_error_EE,RRMS_SIE,RRMS_EE,mixed_RRMS_max_norm_SIE,mixed_RRMS_max_norm_EE,timingSIE,timingEE')
 '''
-
-
-
-pl.show()
+plt.show()
